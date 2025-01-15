dc03eccb74bcdc8f7d9f6e6bb098a789e93bd9c7
Mikael Amare <159163639+Mikael-Amare@users.noreply.github.com>
2025-01-15 18:13:46 -0500

    made changes to backup

--- 3ds_project_backup/source/main.cpp
+++ 3ds_project_backup/source/main.cpp
@@ -1,50 +1,63 @@
 #include <3ds.h>
 #include <cstdio>
 #include <cstdlib>
+#include <cstring>
 
-// Screen size for console
 #define SCREEN_WIDTH 50
-#define SCREEN_HEIGHT 20
+#define SCREEN_HEIGHT 19
 
-// PacMan structure
 struct PacMan {
     int x, y;
     char direction;
+    int score;
 };
 
-// Game maze (simple example)
 const char maze[SCREEN_HEIGHT][SCREEN_WIDTH + 1] = {
-    "##################################################",
-    "#                                                #",
-    "#  ###   ####  ####       ####  ####    ####     #",
-    "#  ###   ####  ####   ##  ####  ####    ####     #",
-    "#        ####  ####   ##                     ####",
-    "#######        ####   ##                     ####",
-    "#######  ####  ####   ##  ####  ####    ####     #",
-    "#        ####  ####   ##  ####  ####    ####     #",
-    "#                                                #",
-    "##################################################"
+    "################################################",
+    "# ............................................ #",
+    "# .###. .#### . #### . . . #### . ####. . ####.#",
+    "# .###. .#### . #### . ## . . . . ####. . ####.#",
+    "# . . . .#### . #### . ## . . . . . . . . . . ##",
+    "####### . . . . #### . ## . . . . . . . . . . ##",
+    "####### .#### . #### . ##  ####  #### . . ####.#",
+    "# . . . .#### . #### . ##  ####  #### . . ####.#",
+    "# . . . . . . . . . . . . . . . . . . . . . . .#",
+    "####### . ######################### . ##########",
+    "# . . . . ### . ### . . # . . . . . . . . . . .#",
+    "# . . . . ### . ### . . #  ####  #### . . ####.#",
+    "# . . . . . . . . . . . . . . . . . . . . . . .#",
+    "####### .#### . ### . # . ### . ####. . .#### .#",
+    "####### .#### . ### . # . . . . . . . . . . . ##",
+    "####### .#### . ### . # . . . . . . . . . . . ##",
+    "#       .#### . ### . # . ### . ### . . . ### .#",
+    "#     # . . . . . . . . . . . . . . . . . . . .#",
+    "################################################"
 };
 
-// Initialize PacMan
-PacMan pacman = {1, 1, 'R'};
+char gameMaze[SCREEN_HEIGHT][SCREEN_WIDTH + 1]; // Mutable maze
+PacMan pacman = {1, 16, 'R', 0};
 
-// Function to draw the maze and PacMan
+void initializeGameMaze() {
+    for (int counter = 0; counter < SCREEN_HEIGHT; counter++) {
+        strncpy(gameMaze[counter], maze[counter], SCREEN_WIDTH + 1);
+    }
+}
+
 void drawMaze() {
     consoleClear();
+    printf("Score: %d\n", pacman.score);
     for (int y = 0; y < SCREEN_HEIGHT; ++y) {
         for (int x = 0; x < SCREEN_WIDTH; ++x) {
             if (x == pacman.x && y == pacman.y) {
                 printf("P");
             } else {
-                printf("%c", maze[y][x]);
+                printf("%c", gameMaze[y][x]);
             }
         }
         printf("\n");
     }
 }
 
-// Function to handle movement
 void movePacMan() {
     int newX = pacman.x;
     int newY = pacman.y;
@@ -56,15 +69,19 @@ void movePacMan() {
         case 'R': newX++; break;
     }
 
-    // Check for collisions with walls
-    if (maze[newY][newX] != '#') {
+    if (gameMaze[newY][newX] != '#') { // Check for walls
         pacman.x = newX;
         pacman.y = newY;
+
+        if (gameMaze[newY][newX] == '.') { // Collect pellet
+            gameMaze[newY][newX] = ' '; // Clear pellet
+            pacman.score += 10;
+        }
     }
 }
 
 int main() {
-    // Initialize services
+    initializeGameMaze();
     gfxInitDefault();
     consoleInit(GFX_TOP, NULL);
 
@@ -72,32 +89,27 @@ int main() {
     printf("Press START to exit.\n");
 
     while (aptMainLoop()) {
-        // Scan for input
         hidScanInput();
         u32 kDown = hidKeysDown();
 
-        // Exit game
         if (kDown & KEY_START) break;
 
-        // Change direction
         if (kDown & KEY_UP) pacman.direction = 'U';
         if (kDown & KEY_DOWN) pacman.direction = 'D';
         if (kDown & KEY_LEFT) pacman.direction = 'L';
         if (kDown & KEY_RIGHT) pacman.direction = 'R';
 
-        // Move PacMan
         movePacMan();
-
-        // Draw the maze
         drawMaze();
 
-        // Wait for the next frame
         gfxFlushBuffers();
         gfxSwapBuffers();
         gspWaitForVBlank();
+
+        // Add a delay (e.g., 200 milliseconds)
+        svcSleepThread(200 * 1000 * 1000); // Delay in nanoseconds
     }
 
-    // Exit services
     gfxExit();
     return 0;
 }
